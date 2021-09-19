be2861894ec6d896722e1c0c78965872088303b6
Your Name <minhakim@mins-mbp.home>
2021-09-19 12:59:34 -0400

    timer add

--- js/script.js
+++ js/script.js
@@ -2,90 +2,90 @@ var codeQuestions = [
     {
         question: "1.What does HTML stand for",
         answers: {
-            a: "Hyperlinks and Text Markup Language",
-            b: "Hyper Text Markup Language",
-            c: "Home Tool Markup Language" 
+            a: "a. Hyperlinks and Text Markup Language",
+            b: "b. Hyper Text Markup Language",
+            c: "c. Home Tool Markup Language" 
         },
         correctAnswer: "a"
     },
     {
         question: "2.Choose the correct HTML element to define important text",
         answers: {
-            a: "&lt;b&gt;",
-            b: "&lt;strong&gt;",
-            c: "&lt;important&gt;"
+            a: "a. &lt;b&gt;",
+            b: "b. &lt;strong&gt;",
+            c: "c. &lt;important&gt;"
         },
         correctAnswer: "b"
     },
     {
         question: "3.How can you make a numbered list?",
         answers: {
-            a: "&lt;ol&gt;",
-            b: "&lt;list&gt;",
-            c: "&lt;ul&gt;"
+            a: "a. &lt;ol&gt;",
+            b: "b. &lt;list&gt;",
+            c: "c. &lt;ul&gt;"
         },
         correctAnswer: "a"
     },
     {
         question: "4.What does CSS stand for?",
         answers: {
-            a: "Cascading Style Sheets",
-            b: "Computer Style Sheets",
-            c: "Colorful Style Sheets"
+            a: "a. Cascading Style Sheets",
+            b: "b. Computer Style Sheets",
+            c: "c. Colorful Style Sheets"
         },
         correctAnswer: "a"
     },
     {
         question: "5.Which property is used to change the background color?",
         answers: {
-            a: "color",
-            b: "bgcolor",
-            c: "background-color"
+            a: "a. color",
+            b: "b. bgcolor",
+            c: "c. background-color"
         },
         correctAnswer: "c"
     },
     {
         question: "6.Which CSS property controls the text size?",
         answers: {
-            a: "text-size",
-            b: "text-style",
-            c: "font-size"
+            a: "a. text-size",
+            b: "b. text-style",
+            c: "c. font-size"
         },
         correctAnswer: "c"
     },
     {
         question: "7.How do you select elements with class name 'test'?",
         answers: {
-            a: "#test",
-            b: ".test",
-            c: "*test"
+            a: "a. #test",
+            b: "b. .test",
+            c: "c. *test"
         },
         correctAnswer: "b"
     },
     {
         question: "8.Where is the correct place to insert a JavaScript?",
         answers: {
-            a: "Both the <head> section and the <body> section are correct",
-            b: "The <head> section",
-            c: "The <body> section"
+            a: "a. Both the &lt;head&gt; section and the &lt;body&gt; section are correct",
+            b: "b. The &lt;head&gt; section",
+            c: "c. The &lt;body&gt; section"
         },
         correctAnswer: "a"
     },
     {
         question: '9.How do you write "Hello World" in an alert box?',
         answers: {
-            a: 'alertBox("Hello World");',
-            b: 'alert("Hello Wrold")',
-            c: 'msg("Hello World")'
+            a: 'a. alertBox("Hello World");',
+            b: 'b. alert("Hello Wrold")',
+            c: 'c. msg("Hello World")'
         },
         correctAnswer: "b"
     },
     {
         question: "10.What is the correct way to write a JavaScript array?",
         answers: {
-            a: 'var colors = ["red", "green", "blue"]',
-            b: 'var colors = (1:"red", 2:"green", 3:"blue")',
-            c: 'var colors = "red", "green", "blue"'
+            a: 'a. var colors = ["red", "green", "blue"]',
+            b: 'b. var colors = (1:"red", 2:"green", 3:"blue")',
+            c: 'c. var colors = "red", "green", "blue"'
         },
         correctAnswer: "a"
     }
@@ -95,11 +95,40 @@ var codeQuestions = [
 var quizDiv = document.getElementById('quiz')
 quizDiv.style.display = "none";
 
+var noTime = document.getElementById('quiz-over')
+noTime.style.display = "none";
+
+var quizComplete = document.getElementById('quiz-complete')
+quizComplete.style.display = "none";
 
 var startDiv = document.getElementById('start')
 startDiv.style.display = "block";
 
+var timer = document.getElementById('timer');
+timer.innerHTML = time;
+var time = 150;
 
+window.onlick = onClick
+function onClick () {
+    startDiv.style.display = "none";
+    quizDiv.style.display = "block";
+
+
+    setInterval(function () {
+        time = time - 1;
+        if (time <= 0) {
+            quizDiv.style.display = "none";
+            timer.style.display = "none";
+            noTime.style.display = "block";
+            
+        } else {
+            timer.innerHTML = time;
+        }
+    }, 1000);
+}
+
+var startBtn = document.getElementById('start-btn')
+startBtn.addEventListener('click', onClick)
 
 
 var currentQuestionIndex = 0;
@@ -120,3 +149,94 @@ var renderQuestion = function (index) {
 }
 renderQuestion(currentQuestionIndex);
 
+
+var correctBtnA = document.getElementById('a');
+var correctBtnB = document.getElementById('b');
+var correctBtnC = document.getElementById('c');
+
+var correctChoiceA = function () {
+    var correctAnswer = codeQuestions[currentQuestionIndex].correctAnswer;
+
+    if (correctAnswer === 'a') {
+        currentQuestionIndex = currentQuestionIndex + 1;
+        if (codeQuestions.length === currentQuestionIndex){
+            var finishedTime = time;
+
+            quizDiv.style.display = "none";
+            timer.style.display = "none";
+            quizComplete.style.display = "block";
+
+            quizComplete.innerHTML = finishedTime;
+        }  else {
+            renderQuestion(currentQuestionIndex);
+        }
+
+    } else {
+        time = time - 10;
+        if (time <= 0) {
+            quizDiv.style.display = "none";
+            timer.style.display = "none";
+            noTime.style.display = "block";
+        }
+    }
+}
+
+var correctChoiceB = function () {
+    var correctAnswer = codeQuestions[currentQuestionIndex].correctAnswer;
+
+    if (correctAnswer === 'b') {
+        currentQuestionIndex = currentQuestionIndex + 1;
+        if (codeQuestions.length === currentQuestionIndex){
+            var finishedTime = time;
+
+            quizDiv.style.display = "none";
+            timer.style.display = "none";
+            quizComplete.style.display = "block";
+
+            quizComplete.innerHTML = finishedTime;
+        }else {
+            renderQuestion(currentQuestionIndex);
+        }
+
+
+    } else {
+        time = time - 10;
+        if (time <= 0) {
+            quizDiv.style.display = "none";
+            timer.style.display = "none";
+            noTime.style.display = "block";
+        }
+    }
+}
+
+var correctChoiceC = function () {
+    var correctAnswer = codeQuestions[currentQuestionIndex].correctAnswer;
+
+    if (correctAnswer === 'c') {
+        currentQuestionIndex = currentQuestionIndex + 1;
+        if (codeQuestions.length === currentQuestionIndex){
+            var finishedTime = time;
+
+            quizDiv.style.display = "none";
+            timer.style.display = "none";
+            quizComplete.style.display = "block";
+
+            quizComplete.innerHTML = finishedTime;
+        } else {
+            renderQuestion(currentQuestionIndex);
+
+        }
+
+    } else {
+        time = time - 10;
+        if (time <= 0) {
+            quizDiv.style.display = "none";
+            timer.style.display = "none";
+            noTime.style.display = "block";
+        }
+    }
+}
+
+correctBtnA.addEventListener('click', correctChoiceA);
+correctBtnB.addEventListener('click', correctChoiceB);
+correctBtnC.addEventListener('click', correctChoiceC);
